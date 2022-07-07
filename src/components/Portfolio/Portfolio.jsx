09f6f3f9bbfbf4473836c37116690782abafd3d2
Gregory Mc-Bit <grigory.2004@yandex.ru>
2022-07-07 23:53:20 +0300

    Initial commit

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -9,25 +9,22 @@ function Portfolio(props) {
         <li className="portfolio__project">
           <a className="portfolio__link" href="https://github.com/Gregory-mcbit/how-to-learn" target="_blank" rel="noreferrer">
             <p className="portfolio__name">Статичный сайт</p>
-            <svg className="portfolio__icon" width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M2.60653 16.5241L14.9645 4.14489L14.9432 13.6903H17.2656V0.181818H3.77841L3.7571 2.48295H13.3026L0.944603 14.8622L2.60653 16.5241Z" fill="white" />
-            </svg>
+            <img className="portfolio__icon" width="18" height="17" viewBox="0 0 18 17" fill="none" alt="стрелка" href="../../images/da.png">
+            </img>
           </a>
         </li>
         <li className="portfolio__project">
           <a className="portfolio__link" href="https://github.com/Gregory-mcbit/russian-travel" target="_blank" rel="noreferrer">
             <p className="portfolio__name">Адаптивный сайт</p>
-            <svg className="portfolio__icon" width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M2.60653 16.5241L14.9645 4.14489L14.9432 13.6903H17.2656V0.181818H3.77841L3.7571 2.48295H13.3026L0.944603 14.8622L2.60653 16.5241Z" fill="white"/>
-            </svg>
+            <img className="portfolio__icon" width="18" height="17" viewBox="0 0 18 17" fill="none" alt="стрелка" href="../../images/da.png">
+            </img>
           </a>
         </li>
         <li className="portfolio__project">
           <a className="portfolio__link" href="https://github.com/Gregory-mcbit/react-mesto-api-full" target="_blank" rel="noreferrer">
             <p className="portfolio__name">Одностраничное приложение</p>
-            <svg className="portfolio__icon" width="18" height="17" viewBox="0 0 18 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M2.60653 16.5241L14.9645 4.14489L14.9432 13.6903H17.2656V0.181818H3.77841L3.7571 2.48295H13.3026L0.944603 14.8622L2.60653 16.5241Z" fill="white"/>
-            </svg>
+            <img className="portfolio__icon" width="18" height="17" viewBox="0 0 18 17" fill="none" alt="стрелка" href="../../images/da.png">
+            </img>
           </a>
         </li>
       </ul>
