ff430a2c7cd3f13e6e88542667ce29d06c77ffcc
Gregory-mcbit <76042617+Gregory-mcbit@users.noreply.github.com>
2022-07-15 00:50:32 +0300

    Update MainApi.js

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://api.mnogomovies.nomoredomains.xyz/";
+const BASE_URL = "https://api.mnogomovies.nomoredomains.xyz";
 const handleResponse = (res) => {
   if (res.ok) return res.json();
   else return Promise.reject(res.status);
